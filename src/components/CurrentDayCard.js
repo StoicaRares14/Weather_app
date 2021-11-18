0533888f388cc50347476603777d613da4fcba2a
User <raresadrians@yahoo.com>
2021-11-18 18:11:31 +0200

    implemented animation for current day card

--- src/components/CurrentDayCard.js
+++ src/components/CurrentDayCard.js
@@ -1,11 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CurrentDayCard() {
+  const [xAxis, setXAxis] = useState(0);
+  const [yAxis, setYAxis] = useState(0);
+  const [trans, setTrans] = useState("all 0.5s ease");
+  const [depthLocation, setDepthLocation] = useState(0);
+  const [depthIcon, setDepthIcon] = useState(0);
+  const [depthDeg, setDepthDeg] = useState(0);
+  const [depthDesc, setDepthDesc] = useState(0);
+  const [depthMinMaxDeg, setDepthMinMaxDeg] = useState(0);
+
+  const handleMouseMove = (e) => {
+    setXAxis((window.innerWidth / 2 - e.pageX) / 25);
+    setYAxis((window.innerHeight / 2 - e.pageY) / 25);
+  };
+
+  const handleMouseLeave = (e) => {
+    setXAxis(0);
+    setYAxis(0);
+    setTrans("all 0.5s ease");
+    setDepthLocation(0);
+    setDepthMinMaxDeg(0);
+    setDepthDeg(0);
+    setDepthIcon(0);
+    setDepthDesc(0);
+  };
+
+  const handleMouseEnter = () => {
+    setTrans("none");
+    setDepthLocation(150);
+    setDepthMinMaxDeg(75);
+    setDepthDeg(100);
+    setDepthIcon(200);
+    setDepthDesc(125);
+  };
+
   return (
-    <div className="container">
-      <div className="card">
-        <h2 className="location">Timisoara,Romania</h2>
-        <div className="icon">
+    <div
+      className="container"
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      onMouseEnter={handleMouseEnter}
+    >
+      <div
+        className="card"
+        style={{
+          transform: `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`,
+          transition: `${trans}`,
+        }}
+      >
+        <h2
+          className="location"
+          style={{
+            transform: `translateZ(${depthLocation}px)`,
+          }}
+        >
+          Timisoara,Romania
+        </h2>
+        <div
+          className="icon"
+          style={{
+            transform: `translateZ(${depthIcon}px)`,
+          }}
+        >
           <div className="circle">
             <img
               src="//cdn.weatherapi.com/weather/64x64/day/122.png"
@@ -13,12 +70,32 @@ function CurrentDayCard() {
             />
           </div>
         </div>
-        <h2 className="deg">20&deg;</h2>
-        <div className="minMaxDeg">
+        <h2
+          className="deg"
+          style={{
+            transform: `translateZ(${depthDeg}px)`,
+          }}
+        >
+          20&deg;
+        </h2>
+        <div
+          className="minMaxDeg"
+          style={{
+            transform: `translateZ(${depthMinMaxDeg}px)`,
+          }}
+        >
           <span className="minDeg">Min: 10&deg;</span>
           <span className="maxDeg">Max: 30&deg;</span>
         </div>
-        <h4 className="desc">Cloudy</h4>
+        <h4
+          className="desc"
+          className="desc"
+          style={{
+            transform: `translateZ(${depthDesc}px)`,
+          }}
+        >
+          Cloudy
+        </h4>
       </div>
     </div>
   );
